c07dbe6e06a6e09ce197de40783828848c6af67b
soyeniyi801 <naturalbeauty801@gmail.com>
2024-03-14 18:40:32 -0400

    hover search button, no results function

--- code-house/main.js
+++ code-house/main.js
@@ -12,24 +12,33 @@ async function fetchUserData(event) {
   let data = await response.json();
 
   console.log(data);
-  const avatar = document.getElementById("avatar");
-  avatar.src = data.avatar_url;
-  const isoDateString = data.created_at;
-  const date = new Date(isoDateString);
-  const options = { day: "numeric", month: "long", year: "numeric" };
-  const formattedDate = date.toLocaleDateString("en-GB", options);
-  document.getElementById("date").textContent = formattedDate;
-  document.getElementById("githubName").textContent = data.name;
-  document.getElementById("login").textContent = `@${data.login}`;
-  document.getElementById("bio").textContent =
-    data.bio || "This profile has no bio";
-  document.getElementById("public_repos").textContent = data.public_repos;
-  document.getElementById("followers").textContent = data.followers;
-  document.getElementById("following").textContent = data.following;
-  document.getElementById("location").textContent = data.location || "Not Available";
-  document.getElementById("twitter").textContent = data.twitter_username || "Not Available";
-  document.getElementById("website").textContent = data.website || "Not Available";
-  document.getElementById("company").textContent = data.company || "Not Available";
+  const noResults = document.getElementById("noResults")
 
-  return data;
+  if(data.message === "Not Found"){
+    console.log('no user found')
+    noResults.textContent = "No results"
+  }else {
+    noResults.textContent = ""
+    const avatar = document.getElementById("avatar");
+    avatar.src = data.avatar_url;
+    const isoDateString = data.created_at;
+    const date = new Date(isoDateString);
+    const options = { day: "numeric", month: "long", year: "numeric" };
+    const formattedDate = date.toLocaleDateString("en-GB", options);
+    document.getElementById("date").textContent = formattedDate;
+    document.getElementById("githubName").textContent = data.name;
+    document.getElementById("login").textContent = `@${data.login}`;
+    document.getElementById("bio").textContent =
+      data.bio || "This profile has no bio";
+    document.getElementById("public_repos").textContent = data.public_repos;
+    document.getElementById("followers").textContent = data.followers;
+    document.getElementById("following").textContent = data.following;
+    document.getElementById("location").textContent = data.location || "Not Available";
+    document.getElementById("twitter").textContent = data.twitter_username || "Not Available";
+    document.getElementById("website").textContent = data.website || "Not Available";
+    document.getElementById("company").textContent = data.company || "Not Available";
+  
+    return data;
+  }
+  
 }
