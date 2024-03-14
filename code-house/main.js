70fb42bbf70ce847392a9586c0e0d757ec74917c
soyeniyi801 <naturalbeauty801@gmail.com>
2024-03-14 11:35:08 -0400

    update html and styles

--- code-house/main.js
+++ code-house/main.js
@@ -1,23 +1,34 @@
-// const inputElement = document.getElementById('userSearch');
-// const submitButton = document.getElementById('searchButton')
-
-// submitButton.addEventListener('click', function(event){
-//     event.preventDefault()
-//     const inputValue = inputElement.value;
-//     console.log(inputValue)
-// })
-
-document.addEventListener('DOMContentLoaded', () => {
-    const searchButton = document.getElementById('form');
-    searchButton.addEventListener('submit', (event) => {fetchUserData(event)});
+document.addEventListener("DOMContentLoaded", () => {
+  const searchButton = document.getElementById("form");
+  searchButton.addEventListener("submit", (event) => {
+    fetchUserData(event);
+  });
 });
 
-async function fetchUserData(event){
-    event.preventDefault()
-    const inputValue = document.getElementById('username').value
-    let response = await fetch(`https://api.github.com/users/${inputValue}`);
-    let data = await response.json();
-    console.log(data)
-    return data
-}
+async function fetchUserData(event) {
+  event.preventDefault();
+  const inputValue = document.getElementById("userInput").value;
+  let response = await fetch(`https://api.github.com/users/${inputValue}`);
+  let data = await response.json();
 
+  console.log(data);
+  const avatar = document.getElementById("avatar");
+  avatar.src = data.avatar_url;
+  const isoDateString = data.created_at;
+  const date = new Date(isoDateString);
+  const options = { day: "numeric", month: "long", year: "numeric" };
+  const formattedDate = date.toLocaleDateString("en-GB", options);
+  document.getElementById("date").textContent = formattedDate;
+  document.getElementById("githubName").textContent = data.name;
+  document.getElementById("login").textContent = `@${data.login}`;
+  document.getElementById("bio").textContent =
+    data.bio || "This profile has no bio";
+  document.getElementById("public_repos").textContent = data.public_repos;
+  document.getElementById("followers").textContent = data.followers;
+  document.getElementById("following").textContent = data.following;
+  document.getElementById("location").textContent = data.location;
+  document.getElementById("twitter").textContent =
+    data.twitter_username || "Not Available";
+
+  return data;
+}
