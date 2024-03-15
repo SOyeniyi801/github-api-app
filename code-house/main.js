e66f0983f1c1ea39fd621783f5c8d7de7bbdfd6d
soyeniyi801 <naturalbeauty801@gmail.com>
2024-03-14 21:20:39 -0400

    light dark mode completed , active url for username

--- code-house/main.js
+++ code-house/main.js
@@ -12,13 +12,13 @@ async function fetchUserData(event) {
   let data = await response.json();
 
   console.log(data);
-  const noResults = document.getElementById("noResults")
+  const noResults = document.getElementById("noResults");
 
-  if(data.message === "Not Found"){
-    console.log('no user found')
-    noResults.textContent = "No results"
-  }else {
-    noResults.textContent = ""
+  if (data.message === "Not Found") {
+    console.log("no user found");
+    noResults.textContent = "No results";
+  } else {
+    noResults.textContent = "";
     const avatar = document.getElementById("avatar");
     avatar.src = data.avatar_url;
     const isoDateString = data.created_at;
@@ -27,18 +27,47 @@ async function fetchUserData(event) {
     const formattedDate = date.toLocaleDateString("en-GB", options);
     document.getElementById("date").textContent = formattedDate;
     document.getElementById("githubName").textContent = data.name;
-    document.getElementById("login").textContent = `@${data.login}`;
+    const userNameLink = document.getElementById("login");
+    userNameLink.textContent = `@${data.login}`;
+    userNameLink.href = data.html_url;
     document.getElementById("bio").textContent =
       data.bio || "This profile has no bio";
     document.getElementById("public_repos").textContent = data.public_repos;
     document.getElementById("followers").textContent = data.followers;
     document.getElementById("following").textContent = data.following;
-    document.getElementById("location").textContent = data.location || "Not Available";
-    document.getElementById("twitter").textContent = data.twitter_username || "Not Available";
-    document.getElementById("website").textContent = data.website || "Not Available";
-    document.getElementById("company").textContent = data.company || "Not Available";
-  
+    document.getElementById("location").textContent =
+      data.location || "Not Available";
+    document.getElementById("twitter").textContent =
+      data.twitter_username || "Not Available";
+    document.getElementById("website").textContent =
+      data.blog || "Not Available";
+    document.getElementById("company").textContent =
+      data.company || "Not Available";
+
     return data;
   }
-  
 }
+
+function toggleTheme() {
+  const body = document.body;
+  const lightMode = document.getElementById("light");
+  const darkMode = document.getElementById("dark");
+  
+
+  darkMode.addEventListener("click", () => {
+    console.log("dark mode clicked");
+    body.classList.add("darkMode");
+    body.classList.remove("lightMode");
+    lightMode.style.display = "block";
+    darkMode.style.display = "none";
+  });
+
+  lightMode.addEventListener("click", () => {
+    console.log("light mode clicked");
+    body.classList.add("lightMode");
+    body.classList.remove("darkMode");
+    darkMode.style.display = "block";
+    lightMode.style.display = "none";
+  });
+}
+toggleTheme();
